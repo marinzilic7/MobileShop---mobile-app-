12dadd293ed7442f2600e7e692e9e0e8686e1299
Contineo <marinzilic507@gmail.com>
2024-01-12 20:02:39 +0100

    new password

--- app/src/main/java/com/example/mobileshop/AccountActivity.java
+++ app/src/main/java/com/example/mobileshop/AccountActivity.java
@@ -1,17 +1,23 @@
 package com.example.mobileshop;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -33,6 +39,7 @@ public class AccountActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = auth.getCurrentUser();
+
         TextView emailTextView = findViewById(R.id.userEmail);
         TextView nameTextView = findViewById(R.id.userName);
 
@@ -58,6 +65,40 @@ public class AccountActivity extends AppCompatActivity {
 
     }
 
+    public void potvrdiReset(View view){
+        EditText novaLozinka = findViewById(R.id.novaLozinka);
+        String lozinka = novaLozinka.getText().toString();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(lozinka.isEmpty()){
+            Toast.makeText(this, "Molimo unesite novu lozinku", Toast.LENGTH_SHORT).show();
+            return;
+        }else{
+            user.updatePassword(lozinka).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if(task.isSuccessful()){
+                        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users").child(user.getUid());
+                        databaseReference.child("password").setValue(lozinka);
+                        Toast.makeText(AccountActivity.this, "Promijenjena lozinka", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(AccountActivity.this, "Greska", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
 
+    }
 
+    public void updateProfile(View view){
+        openModal();
+    }
+
+    private void openModal(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        View DialogView = inflater.inflate(R.layout.modal, null);
+        builder.setView(DialogView);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }

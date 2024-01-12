36340fd0c6638c1d34751f3c8326655b0af98c62
Contineo <marinzilic507@gmail.com>
2024-01-12 17:05:17 +0100

    created burger for nav

---
+++ app/src/main/java/com/example/mobileshop/AccountActivity.java
@@ -0,0 +1,26 @@
+package com.example.mobileshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class AccountActivity extends AppCompatActivity {
+
+    private  DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.account);
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/mobileshop/HomeActivity.java
+++ app/src/main/java/com/example/mobileshop/HomeActivity.java
@@ -1,34 +1,58 @@
 package com.example.mobileshop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private  DrawerLayout drawer;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
         drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                handleNavigationItemSelected(item);
+                return true;
+            }
+        });
     }
-    public void openDrawer(View view){
+
+    public void handleNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        final int ACCOUNT_MENU_ID = R.id.account;
+
+        if (id == ACCOUNT_MENU_ID) {
+            Intent intent = new Intent(this, AccountActivity.class);
+            startActivity(intent);
+        }
+
+        closeDrawer();
+    }
+
+    public void openDrawer(View view) {
         drawer.openDrawer(GravityCompat.START);
     }
 
-    public void closeDrawer(){
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+    public void closeDrawer() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         }
     }
-
-
 }
